f0fe2f9a74068954fceedbf96ba72b945795ba64
Michael W <hotdog003@gmail.com>
2010-07-03 21:02:00 -0600

    incorrectly calculated number of users

--- client.js
+++ client.js
@@ -67,7 +67,7 @@ client.addListener('joined', function() {
         split=true;
       }
     }
-    sys.print("\n-- Total of " + (numUsers+numOps) + " users online ("+numOps+" ops)\n");
+    sys.print("\n-- Total of " + (numUsers) + " users online ("+numOps+" ops)\n");
     firstConnection = false;
   });
 
